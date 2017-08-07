0780e3c386a3badc179d3b77518b3da213f162eb
JozoSlejko <jozo.slejko@gmail.com>
2017-08-07 16:10:05 +0200

    First commit

---
+++ azureAppAD/Models/WorkItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace azureAppAD.Models
+{
+    public class WorkItem
+    {
+        [Key]
+        public int ItemID { get; set; }
+        public string AssignedToID { get; set; }
+        public string AssignedToName { get; set; }
+        public string Description { get; set; }
+        public WorkItemStatus Status { get; set; }
+    }
+
+    public enum WorkItemStatus
+    {
+        Open,
+        Investigating,
+        Resolved,
+        Closed
+    }
+    }
